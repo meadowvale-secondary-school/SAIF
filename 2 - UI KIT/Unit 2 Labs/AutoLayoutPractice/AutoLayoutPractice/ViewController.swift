3937f1cec4e0a2f8701ea2af625be3d171f653ab
Saif-Aldeen Abu-Osba - Meadowvale SS (2562) <746599@pdsb.net>
2019-04-16 09:07:19 -0400

    The following commit contains all 2.10 work

---
+++ 2 - UI KIT/Unit 2 Labs/AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Student07 on 2019-04-11.
+//  Copyright © 2019 Student07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 2 - UI KIT/Unit 2 Labs/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Student07 on 2019-04-11.
+//  Copyright © 2019 Student07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
