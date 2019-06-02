2c5667ca74a4137d180ddcf61bb45c26195977fe
Saif-Aldeen Abu-Osba - Meadowvale SS (2562) <746599@pdsb.net>
2019-06-02 13:22:16 -0400

    Completed Guided Project

--- 3 - Navigation/PersonalityQuiz/PersonalityQuiz/QuestionData.swift
+++ 3 - Navigation/PersonalityQuiz/PersonalityQuiz/QuestionData.swift
@@ -33,19 +33,29 @@ enum AnimalType: Character {
     var definition: String {
         switch self {
         case .dog:
-            return "You are incredibly outgoing. You surround yourself with the people you love and enjoy activities with your friends."
+            return "You are lovable and enjoy being surrounded by freinds and family.."
         case .cat:
-            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms."
+            return "You are mild-tempered and enjoy doing things on your own terms."
         case .rabbit:
-            return "You love everything that's soft. You are healthy and full of energy."
+            return "You are healthy and full of energy, yet quite scared all the time."
         case .turtle:
-            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+            return "You are wise, slow and steady wins the race."
         }
     }
 }
 
 var questions:
-    [Question] = [Question(text: "How playful are you?", type: .range,
+    [Question] = [
+        Question(text: "How Athletic Are You?", type: .single,
+            answers: [
+                Answer(text: "Not Athletic At All", type: .turtle),
+                Answer(text: "Somewhat Athletic", type: .cat),
+                Answer(text: "Athletic", type: .rabbit),
+                Answer(text: "Very Athletic", type: .dog)
+        ]
+        ),
+        
+        Question(text: "How playful are you?", type: .range,
              answers: [
                 Answer(text: "Quite Playful", type: .cat),
                 Answer(text: "Jumpy", type: .rabbit),
@@ -53,26 +63,23 @@ var questions:
                 Answer(text: "Very Playful", type: .dog)
         ]
         ),
-                
-    Question(text: "How Athletic Are You?", type: .multiple,
-             answers: [
-                Answer(text: "Not Athletic At All", type: .turtle),
-                Answer(text: "Somewhat Athletic", type: .cat),
-                Answer(text: "Athletic", type: .rabbit),
-                Answer(text: "Very Athletic", type: .dog)
-        ]
-    ),
 
-     Question(text: "Which food do you like the most?", type: .single,
-                        answers: [
-                            Answer(text: "Steak", type: .dog),
-                            Answer(text: "Fish", type: .cat),
-                            Answer(text: "Carrots", type: .rabbit),
-                            Answer(text: "Corn", type: .turtle)
-    ]
-    )
+     Question(text: "Which food do you like the most?", type: .multiple,
+            answers: [
+                Answer(text: "Steak", type: .dog),
+                Answer(text: "Fish", type: .cat),
+                Answer(text: "Carrots", type: .rabbit),
+                Answer(text: "Corn", type: .turtle)
+])]
+
+
+
     
+var answersChosen: [Answer] = []
+
+
+
    
     
-]
+
 
--- 3 - Navigation/PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
+++ 3 - Navigation/PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -12,19 +12,34 @@ class ResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    
+    var responses: [Answer]!
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        for response in responseTypes {
+            frequencyOfAnswers[response] = (frequencyOfAnswers[response]
+                ?? 0) + 1
+        }
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+            { (pair1, pair2) -> Bool in
+                return pair1.value > pair2.value
+        })
+        let mostCommonAnswer = frequentAnswersSorted.sorted { $0.1 >
+            $1.1 }.first!.key
+    
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
     }
-    */
+
 
 }
